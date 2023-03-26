55d89e4e3ae673ea8ba473e7ec204739b7680faa
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-03-26 13:00:11 +0400

    i have compelee the featching from the Jason file and save the information in the database

---
+++ readJasonFilepackage/MyObject.java
@@ -0,0 +1,52 @@
+package readJasonFilepackage;
+
+import java.util.List;
+
+class MyObject {
+	private String name;
+    private String country;
+    private String stateProvince;
+    private List<String> domains;
+    private List<String> web_pages;
+    private String alpha_two_code;
+    
+    public String getName() {
+        return name;
+}
+    public void setName(String name) {
+        this.name = name;
+}
+    public String getCountry() {
+        return country;
+}
+    public void setCountry(String country) {
+        this.country = country;
+}
+    public String getStateProvince() {
+        return stateProvince;
+}
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+}
+    public List<String> getDomains() {
+        return domains;
+}
+    public void setDomains(List<String> domains) {
+        this.domains = domains;
+}
+    public List<String> getWebPages() {
+        return web_pages;
+}
+    public void setWebPages(List<String> webPages) {
+        this.web_pages = webPages;
+}
+    public String getAlphaTwoCode() {
+        return alpha_two_code;
+}
+    public void setAlphaTwoCode(String alphaTwoCode) {
+        this.alpha_two_code = alphaTwoCode;
+}
+}
+
+
+
